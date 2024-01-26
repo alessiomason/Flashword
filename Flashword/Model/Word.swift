0c271c119fc3df5905e09e51322361168aca4da5
Alessio Mason <alessio.mason@me.com>
2024-01-26 18:32:15 +0100

    Localizable string in Word model

--- Flashword/Model/Word.swift
+++ Flashword/Model/Word.swift
@@ -16,7 +16,8 @@ class Word {
     @Relationship(inverse: \Category.words) var category: Category?
     
     var categoryName: String {
-        category?.name ?? "No category"
+        let localizedNoCategory = String(localized: "No category", comment: "The text to display in absence of a user-defined category")
+        return category?.name ?? localizedNoCategory
     }
     
     init(term: String, learntOn: Date, notes: String = "", category: Category? = nil) {
@@ -28,5 +29,6 @@ class Word {
     
     #if DEBUG
     static let example = Word(term: "Swift", learntOn: .now, notes: "A swift testing word.")
+    static let otherExample = Word(term: "Apple", learntOn: .now.addingTimeInterval(-86400), notes: "A fruit or a company?")
     #endif
 }
